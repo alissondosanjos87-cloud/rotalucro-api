202ba2600e8c4475b98ca0cc59d69aaa1f840114
alissondosanjos87-cloud <alissondosanjos87@gmail.com>
2026-05-06 15:58:15 -0300

    Update nearestNeighbor.js

--- otimizador/nearestNeighbor.js
+++ otimizador/nearestNeighbor.js
@@ -1,2 +1,41 @@
-const {distancia}=require('./utils');
-module.exports=function(p){if(!p||p.length<2)return p;const r=[...p],rota=[];let a=r.shift();rota.push(a);while(r.length){let bi=0,bs=1e9;for(let i=0;i<r.length;i++){const s=distancia(a,r[i]);if(s<bs){bs=s;bi=i;}}a=r.splice(bi,1)[0];rota.push(a);}return rota;};
+// otimizador/nearestNeighbor.js
+var { distancia } = require('./utils');
+var calcularScore = require('./score');
+
+function nearestNeighbor(paradas, startIndex) {
+  if (!paradas || paradas.length <= 1) return paradas ? paradas.slice() : [];
+
+  var rest = paradas.slice();
+  var rota = [];
+  var atual = rest.splice(startIndex || 0, 1)[0];
+  rota.push(atual);
+
+  var hora = new Date().getHours();
+
+  while (rest.length > 0) {
+    var melhor = 0;
+    var melhorScore = Infinity;
+
+    for (var i = 0; i < rest.length; i++) {
+      var d = distancia(atual, rest[i]);
+      var score = calcularScore({
+        distancia: d,
+        tempoParada: rest[i].tempoParada || 5,
+        hora: hora,
+        tipo: rest[i].tipo || 'casa'
+      });
+
+      if (score < melhorScore) {
+        melhorScore = score;
+        melhor = i;
+      }
+    }
+
+    atual = rest.splice(melhor, 1)[0];
+    rota.push(atual);
+  }
+
+  return rota;
+}
+
+module.exports = nearestNeighbor;
