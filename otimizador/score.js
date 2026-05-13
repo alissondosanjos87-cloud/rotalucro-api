3f1d2f2399915a345c36edd5832aec82fd196481
alissondosanjos87-cloud <alissondosanjos87@gmail.com>
2026-05-12 23:19:52 -0300

    Update score.js

--- otimizador/score.js
+++ otimizador/score.js
@@ -1,5 +1,5 @@
 // otimizador/score.js
-function calcularScore({ distancia, tempoParada = 5, hora = 12, tipo = 'casa' }) {
+function calcularScore({ distancia, tempoParada = 5, hora = 12, tipo = 'casa', bairro = '', bairroAtual = '' }) {
   let fator = 1;
 
   if (hora >= 17 && hora <= 19) fator = 1.4;
@@ -9,8 +9,10 @@ function calcularScore({ distancia, tempoParada = 5, hora = 12, tipo = 'casa' })
     tipo === 'condominio' ? 1.5 :
     tipo === 'apto' ? 1.2 : 1;
 
+  const bonusBairro = (bairro && bairroAtual && bairro === bairroAtual) ? 0.8 : 1;
+
   return (
-    distancia * 0.65 +
+    distancia * 0.65 * bonusBairro +
     tempoParada * 0.25 * pesoTipo +
     distancia * fator * 0.1
   );
